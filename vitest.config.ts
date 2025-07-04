fe910d8c4b6a29a35a4b31be460c06e249f6d161
Krzysztof Wojnar <krzysztofwojnar@outlook.com>
2025-07-04 21:13:11 +0200

    old node 20 versions support

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vitest/config';
-import { resolve } from 'path';
+import path, { resolve } from 'path';
+import { fileURLToPath } from "url";
+
+// Node.js compatibility for __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   test: {
